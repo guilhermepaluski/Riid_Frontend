f11cd10051c585a2421336c8906aa4112df9383f
Gustavo <gupaluski@gmail.com>
2025-06-28 13:04:38 -0300

    feat: create modal not logged

--- src/components/ModalNotLogged.jsx
+++ src/components/ModalNotLogged.jsx
@@ -1,11 +1,11 @@
-import style from '../styles/ModalNotLogged.css';
+import style from '../styles/ModalNotLogged.module.css';
 
 const ModalNotLogged = () => {
     return(
         <div className={style.backgroundBlur}>
-            <div className='atentionCard'>
+            <div className= {style.atentionCard}>
                 <span>Parece que você não está logado! Faça o login para continuar essa ação</span>
-                <button className='btnClass'>Close</button>
+                <button className={style.btnClass}>Close</button>
             </div>
         </div>
     );
