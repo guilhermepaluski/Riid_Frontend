7fe797d838d83781bb7689bbb3cedd8484315030
Gustavo <gupaluski@gmail.com>
2025-06-29 19:27:02 -0300

    feat: open pdf in other tab

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -1,9 +1,9 @@
 import "react";
 
-const DownloadButton = () => {
+const DownloadButton = ({handleClick}) => {
     return (
         <div>
-            <button className="flex gap-5 font-bold text-white text-3xl bg-black px-10 py-4 rounded-4xl cursor-pointer">
+            <button onClick={handleClick} className="flex gap-5 font-bold text-white text-3xl bg-black px-10 py-4 rounded-4xl cursor-pointer">
                 <img src="/images/downloadpnglogo.png" alt="" className="w-10" />
                 Download PDF
             </button>
--- src/pages/DownloadBookPage.jsx
+++ src/pages/DownloadBookPage.jsx
@@ -21,34 +21,56 @@ const DownloadBookPage = () => {
             fetchLoans();
     }, []);
 
+    const handleDownloadBook = async () => {
+        try {
+            const response = await api.get(`BookPdf/download/${id}`, {
+                responseType: 'blob'
+            });
+
+            const blobUrl = window.URL.createObjectURL(new Blob([response.data], { type: 'application/pdf' }));
+
+            window.open(blobUrl, '_blank');
+        } catch (err) {
+            console.error("Erro ao abrir o PDF:", err);
+            setError("Erro ao abrir o PDF");
+        }
+    };
+
     if (!book) return <div className="p-8">Carregando...</div>;
 
     return (
-        <div className="min-h-screen flex flex-col" style={{ backgroundColor: 'rgb(243, 237, 233)' }}>
+        <div className="min-h-screen flex w-full" style={{ backgroundColor: 'rgb(243, 237, 233)' }}>
 
             <div className="flex p-15 min-h-screen">
                 {/* BookImage */}
-                <div> 
-                    <img src= { book.book_Image } alt={book.image} className="w-[400px] mr-35 shadow-2xl" />
+                <div className="w-1/3"> 
+                    <img src= { book.book_Image } alt={book.image} className="w-[400px] shadow-2xl" />
                 </div>
 
                 {/* Book Infos */}
-                <div className="space-y-4">
+                <div className="space-y-4 w-2/3 flex-col justify-center items-center">
                     
                     {/* Title */}
                     <div className="text-4xl font-bold">
                         <h1>{ book.book_Name }</h1>
                     </div>
 
-                    {/* Description 
                     <div className="text-2xl">
-                        <h1>{ book.category }</h1>
+                        <h2>{ `Author: ${book.book_Author}` }</h2>
                     </div>
-                    */}
+
+                    {/* Description*/}
+                    <div className="text-2xl">
+                        <h2>{ `Category: ${book.book_Category}` }</h2>
+                    </div>
+
+                    <div className="text-sm">
+                        <h2>{book.book_Description}</h2>
+                    </div>
 
                     {/* DownloadButton */}
                     <div className="flex justify-start gap-25 items-center h-40">
-                        <DownloadButton bookId={book.id} />
+                        <DownloadButton handleClick={handleDownloadBook} />
                     </div>
                 </div>
             </div>
